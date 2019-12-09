75a762bfca6525f10c7b98f6ed9f349c222b31c2
Ibrahima Niass <cheroubawan@hotmail.fr>
2019-12-09 19:30:15 +0100

    fetch one user by id

--- src/index.js
+++ src/index.js
@@ -21,6 +21,28 @@ app.post('/users', (req, res) => {
   // res.send('Testing!');
 });
 
+app.get('/users', (req, res)=> {
+  User.find({}).then((users)=> {
+    res.send(users)
+  }).catch(e=> {
+    res.send(500).send()
+  })
+})
+
+app.get('/users/:id', (req,res)=> {
+  const _id = req.params.id;  
+  User.findById(_id).then(user=> {
+    if(!user) {
+      return res.status(404).send()
+    }
+    res.send(user)
+
+  }).catch(e=> {
+    console.log(e)
+    res.status(500).send()
+  }) 
+})
+
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body);
   task
