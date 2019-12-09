39be6805c535c50e9003d9c04506f60b26049ea8
Ibrahima Niass <cheroubawan@hotmail.fr>
2019-12-09 19:39:46 +0100

    set endpoints to fetch all tasks or task by id

--- src/index.js
+++ src/index.js
@@ -21,27 +21,30 @@ app.post('/users', (req, res) => {
   // res.send('Testing!');
 });
 
-app.get('/users', (req, res)=> {
-  User.find({}).then((users)=> {
-    res.send(users)
-  }).catch(e=> {
-    res.send(500).send()
-  })
-})
+app.get('/users', (req, res) => {
+  User.find({})
+    .then(users => {
+      res.send(users);
+    })
+    .catch(e => {
+      res.send(500).send();
+    });
+});
 
-app.get('/users/:id', (req,res)=> {
-  const _id = req.params.id;  
-  User.findById(_id).then(user=> {
-    if(!user) {
-      return res.status(404).send()
-    }
-    res.send(user)
-
-  }).catch(e=> {
-    console.log(e)
-    res.status(500).send()
-  }) 
-})
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch(e => {
+      console.log(e);
+      res.status(500).send();
+    });
+});
 
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body);
@@ -54,6 +57,31 @@ app.post('/tasks', (req, res) => {
       res.status(400).send(e);
     });
   // res.send('Testing!');
+});
+
+app.get('/tasks', (req, res) => {
+  Task.find({})
+    .then(tasks => {
+      res.send(tasks);
+    })
+    .catch(e => {
+      res.send(500).send();
+    });
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id)
+    .then(task => {
+      if (!task) {
+        return res.status(404).send();
+      }
+      res.send(task);
+    })
+    .catch(e => {
+      console.log(e);
+      res.status(500).send();
+    });
 });
 
 app.listen(port, () => {
