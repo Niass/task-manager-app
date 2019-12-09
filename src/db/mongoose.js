c892622ae7a8853c9f54607d73be0d79425d1259
Ibrahima Niass <cheroubawan@hotmail.fr>
2019-12-09 18:16:10 +0100

    Set up user creation endpoint

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -7,72 +6,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useUnifiedTopology: true
 });
 
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    trim:true
-  },
-  email: {
-    type: String,
-    required:true,
-    trim:true,
-    lowercase:true,
-    validate(value){
-      if(!validator.isEmail(value)){
-        throw new Error('Email is invalid')
-      }
-    }
-  },
-  password:{
-    type: String,
-    minlength:7,
-    required:true,
-    trim:true,
-    validate(value){
-      if(value.toLowerCase( ).includes('password')) {
-        throw new Error('must not include password')
-      }
-    }
-  },
-  age: {
-    type: Number,
-    default:0,
-    validate(value) {
-      if (value < 0) {
-        throw Error('Age must be a positive number');
-      }
-    }
-  }
-});
 
 const Task = mongoose.model('Task', {
   description: {
-    type: String
+    type: String,
+    require:true,
+    trim:true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default:false
   }
 });
-const me = new User({
-  name: 'Fred',
-  email: 'hello@gmail.com'
-});
-
-const myTask = new Task({
-  description: 'master react',
-  completed: false
-});
-
-// myTask
-//   .save()
-//   .then(result => console.log(result))
-//   .catch(error => console.log(error));
-
-me.save()
-  .then(response => {
-    console.log(response);
-  })
-  .catch(error => {
-    console.log('Error ', error);
-  });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
+require('./db/mongoose');
+const User = require('./models/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+  const user = new User(req.body);
+  user
+    .save()
+    .then(() => {
+      res.send(user);
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
+  // res.send('Testing!');
+});
 
 app.listen(port, () => {
   console.log('Server is running up on port ' + port);
