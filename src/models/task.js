f69031fcb9870b4da209ac69b348e60eaf51585e
Ibrahima Niass <cheroubawan@hotmail.fr>
2019-12-12 17:15:04 +0100

    sort task completed or not query string

--- src/models/task.js
+++ src/models/task.js
@@ -1,29 +1,38 @@
 const mongoose = require('mongoose');
 
-const taskSchema = new mongoose.Schema({
-  description: {
-    type: String,
-    require: true,
-    trim: true
+const taskSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      require: true,
+      trim: true
+    },
+    completed: {
+      type: Boolean,
+      default: false
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    }
   },
-  completed: {
-    type: Boolean,
-    default: false
+  {
+    timestamps: true
+  }
+);
+taskSchema.pre(
+  'save',
+  async function(next) {
+    const task = this;
+    console.log('What is next');
+    if (task.isModified('password')) {
+      task.password = await bcrypt.hash(task.password, 8);
+    }
+    next();
   },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User'
-  }
-});
-taskSchema.pre('save', async function(next) {
-  const task = this;
-  console.log('What is next');
-  if (task.isModified('password')) {
-    task.password = await bcrypt.hash(task.password, 8);
-  }
-  next();
-});
+  {}
+);
 const Task = mongoose.model('Task', taskSchema);
 
 module.exports = Task;
