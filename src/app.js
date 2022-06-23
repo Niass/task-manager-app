66d4cf63f76b8cb17b11245af0e93f9908522769
Niass <cheroubawan@hotmail.fr>
2022-06-23 15:19:27 +0200

    update

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 require('./db/mongoose');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
+const prebidRouter = require('./routers/prebid');
 
 const app = express();
 
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+app.use(prebidRouter);
 
 module.exports = app;
